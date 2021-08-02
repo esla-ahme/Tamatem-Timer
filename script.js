51d93938b347a5cd13d4677e576c2fc4d7c08ace
eslam <esla.ahme@hotmail.com>
2021-08-02 16:39:01 +0200

    timer is working continousely

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const MODES = ["pomodoro", "shortBreak", "longBreak"]
 const settings = {
   color: "#ea4831",
   pomodoro: 25,
-  shortBreak: 10,
+  shortBreak: 1,
   longBreak: 15,
   selected: 0
 }
+let currentTimerValue, timerInSec, progress;
 
 /*Selectors*/
 
@@ -28,21 +29,50 @@ pomoTime = document.querySelector('.pomodoro-time')
 shortTime = document.querySelector('.short-break')
 longTime = document.querySelector('.long-break')
 
-
+//timeProgressBar 
+timeProgressBar = document.querySelector('.sec-circle')
 /*=======LOGIC=======*/
 if (document) {
   pomoTime.value = settings["pomodoro"]
   shortTime.value = settings["shortBreak"]
   longTime.value = settings["longBreak"]
   time.innerText = settings["pomodoro"] + ":00"
-
+  resetTime()
 }
 /*Timer*/
 
-const setTime = (mode) => {
-  time.innerText = settings[mode] + ":00"
+const setTime = (mode, sec = "00") => {
+  time.innerText = settings[mode] + ":" + sec
 }
 
+
+function resetTime() {
+  timerInSec = 60 * Number(settings[MODES[settings["selected"]]])
+  currentTimerValue = timerInSec;
+  progress = 0;
+  timeProgressBar.style.strokeDasharray = `calc(${progress}*754/100),754px`
+
+}
+function changeTime() {
+  if (currentTimerValue == 0) {
+    var audio = new Audio('alarm.wav');
+    audio.play();
+    resetTime()
+    return
+  }
+  currentTimerValue--;
+
+  min = Math.floor(currentTimerValue / 60);
+  sec = currentTimerValue % 60;
+  time.innerText = `${min}:${sec}`
+
+}
+setInterval(changeTime, 1000);
+setInterval(() => {
+  progress = Math.ceil((timerInSec - currentTimerValue) / timerInSec * 100)
+  timeProgressBar.style.strokeDasharray = `calc(${progress}*754/100),754px`
+
+}, 5000)
 
 
 
@@ -64,6 +94,7 @@ clickedMode.addEventListener('click',
         settings["selected"] = 2;
 
       setTime(MODES[settings["selected"]])
+      resetTime()
     }
   }
 )
@@ -85,7 +116,8 @@ const handleApply = () => {
   settings["pomodoro"] = Number(pomoTime.value)
   settings["shortBreak"] = Number(shortTime.value)
   settings["longBreak"] = Number(longTime.value)
-  console.log(longTime.value)
+  setTime(MODES[settings["selected"]])
+  resetTime()
   toggleModel()
 }
 
