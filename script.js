c70acce835897520e07bd94cc97c2a4e29506053
eslam <esla.ahme@hotmail.com>
2021-08-02 09:47:21 +0200

    1- handlled all settings changes 2- improved respomsevness

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
+
+/*====settings object*/
+const MODES = ["pomodoro", "shortBreak", "longBreak"]
+const settings = {
+  color: "#ea4831",
+  pomodoro: 25,
+  shortBreak: 10,
+  longBreak: 15,
+  selected: 0
+}
+
 /*Selectors*/
+
 /*Nav Selectors */
 const clickedMode = document.querySelector('.mode-lst')
 
-
+/*Timer selectors*/
+time = document.querySelector('.time')
 
 /*Modal Selectors*/
 const icons = document.querySelectorAll('.icon')
 const modal = document.querySelector('.modal')
+//color selector
+colors = document.querySelector('.colors')
 
+//times
+pomoTime = document.querySelector('.pomodoro-time')
+shortTime = document.querySelector('.short-break')
+longTime = document.querySelector('.long-break')
+
+
+/*=======LOGIC=======*/
+if (document) {
+  pomoTime.value = settings["pomodoro"]
+  shortTime.value = settings["shortBreak"]
+  longTime.value = settings["longBreak"]
+  time.innerText = settings["pomodoro"] + ":00"
+
+}
 /*Timer*/
+
+const setTime = (mode) => {
+  time.innerText = settings[mode] + ":00"
+}
+
 
 
 
@@ -21,6 +55,14 @@ clickedMode.addEventListener('click',
     modeActive.classList.remove('active')
     //add active class to clicked mode 
     e.target.classList.add('active');
+    if (e.target.innerText[0] == 'p')
+      settings["selected"] = 0;
+    else if (e.target.innerText[0] == 's')
+      settings["selected"] = 1;
+    else
+      settings["selected"] = 2;
+
+    setTime(MODES[settings["selected"]])
   }
 )
 
@@ -35,3 +77,27 @@ function toggleModel() {
   showModal ? modal.style.display = "none" : modal.style.display = "block";
   showModal = !showModal;
 }
+
+
+const handleApply = () => {
+  settings["pomodoro"] = Number(pomoTime.value)
+  settings["shortBreak"] = Number(shortTime.value)
+  settings["longBreak"] = Number(longTime.value)
+  console.log(longTime.value)
+  toggleModel()
+}
+
+document.querySelector('.apply').addEventListener('click', handleApply)
+/*Change color */
+colors.addEventListener('click',
+  (e) => {
+    //select active mode 
+    const activeColor = document.querySelector('.active-color')
+    //remove active class
+    activeColor.classList.remove('active-color')
+    //add active class to clicked mode 
+    e.target.classList.add('active-color');
+    settings["color"] = getComputedStyle(e.target).backgroundColor
+    document.documentElement.style.setProperty("--sc", settings["color"])
+  }
+)
